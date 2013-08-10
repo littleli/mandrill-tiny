af7e97f5d4066f4efbe2c330c2bd412eef682bb1
Aleš Najmann <ales.najmann@gmail.com>
2013-08-10 02:07:22 +0200

    cleanup, move to ArrayList as it is lighter and has better indexed access

--- src/main/java/cz/najmann/mandrill/api10/Struct.java
+++ src/main/java/cz/najmann/mandrill/api10/Struct.java
@@ -2,12 +2,6 @@ package cz.najmann.mandrill.api10;
 
 import java.util.LinkedHashMap;
 
-/**
- *
- * User: littleli
- * Date: 1.8.13
- * Time: 23:25
- */
 public final class Struct extends LinkedHashMap<String, Object> {
 
     public Struct set(String name, Object o) {
--- src/main/java/cz/najmann/mandrill/api10/StructArray.java
+++ src/main/java/cz/najmann/mandrill/api10/StructArray.java
@@ -1,13 +1,8 @@
 package cz.najmann.mandrill.api10;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
-/**
- * User: littleli
- * Date: 3.8.13
- * Time: 0:43
- */
-public final class StructArray extends LinkedList<Struct> {
+public final class StructArray extends ArrayList<Struct> {
 
     public static StructArray with(Struct struct) {
         return (new StructArray()).append(struct);
