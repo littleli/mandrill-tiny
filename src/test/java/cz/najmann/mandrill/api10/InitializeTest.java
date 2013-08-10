eaad225c2ccb0616d4b084d4d04d90395e8793ae
Aleš Najmann <ales.najmann@gmail.com>
2013-08-11 01:11:32 +0200

    NPE tests

--- src/test/java/cz/najmann/mandrill/api10/InitializeTest.java
+++ src/test/java/cz/najmann/mandrill/api10/InitializeTest.java
@@ -1,11 +1,32 @@
 package cz.najmann.mandrill.api10;
 
-/**
- * Created with IntelliJ IDEA.
- * User: littleli
- * Date: 11.08.13
- * Time: 0:53
- * To change this template use File | Settings | File Templates.
- */
+import cz.najmann.mandrill.api10.http.HttpComponentsHandler;
+import cz.najmann.mandrill.api10.http.JettyHttpHandler;
+import cz.najmann.mandrill.api10.json.GsonJsonHandler;
+import cz.najmann.mandrill.api10.json.JacksonJsonHandler;
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class InitializeTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testGsonJsonHandler() {
+        new GsonJsonHandler(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testJacksonJsonHandler() {
+        new JacksonJsonHandler(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testApacheHttpComponents() {
+        new HttpComponentsHandler(null);
+    }
+
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void testJettyHttpHandler() {
+        new JettyHttpHandler(null);
+    }
 }
