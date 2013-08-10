b4302f5193b2faec0928844b172dfe4c8ec2cbb5
Aleš Najmann <ales.najmann@gmail.com>
2013-08-11 01:07:12 +0200

    Mockito added

---
+++ src/test/java/cz/najmann/mandrill/api10/InitializeTest.java
@@ -0,0 +1,11 @@
+package cz.najmann.mandrill.api10;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: littleli
+ * Date: 11.08.13
+ * Time: 0:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class InitializeTest {
+}
