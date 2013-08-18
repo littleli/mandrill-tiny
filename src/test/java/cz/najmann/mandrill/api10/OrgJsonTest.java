86c79b89d53e27c1beccb26b2a3f499ff8a2646e
Aleš Najmann <ales.najmann@gmail.com>
2013-08-18 14:04:25 +0200

    Null and Unexpected type failure

--- src/test/java/cz/najmann/mandrill/api10/OrgJsonTest.java
+++ src/test/java/cz/najmann/mandrill/api10/OrgJsonTest.java
@@ -1,5 +1,6 @@
 package cz.najmann.mandrill.api10;
 
+import cz.najmann.mandrill.api10.json.JsonError;
 import cz.najmann.mandrill.api10.json.OrgJsonHandler;
 import org.junit.Test;
 
@@ -28,17 +29,28 @@ public class OrgJsonTest extends ParseTest {
     }
 
     @Test
-    public void testParseString() {
+    public void testStringToJson() {
         assertEquals("pat", jsonHandler.toJson("pat"));
     }
 
     @Test
-    public void testCollection() {
+    public void testCollectionToJson() {
         assertEquals("[1,2,3]", jsonHandler.toJson(Arrays.asList(1, 2, 3)));
     }
 
     @Test
-    public void testMap() {
+    public void testMapToJson() {
         assertEquals("{\"key\":\"value\"}", jsonHandler.toJson(Collections.singletonMap("key", "value")));
+    }
+
+    @Test(expected = JsonError.class)
+    public void testNullToJson() {
+        jsonHandler.toJson(null);
+    }
+
+    @Test(expected = JsonError.class)
+    public void testUnexpectedToJson() {
+        Error error = new Error();
+        jsonHandler.toJson(error);
     }
 }
